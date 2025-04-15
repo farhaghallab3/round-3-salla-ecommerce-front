5421f398ad549dc022cad1ff12f3c960b9e51e6a
AhmedEssam <ae.devverse@gmail.com>
2025-04-16 01:03:23 +0200

    add new changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,26 @@
+import AdsSection from "../components/homeComponents/AdsSection";
 import { FeaturesSection } from "../components/homeComponents/FeaturesSection";
+import HeroSection from "../components/homeComponents/HeroSection";
 import { LapBanner } from "../components/homeComponents/LapBanner";
+import SlidersSection from "../components/homeComponents/SlidersSection";
 import { SpecialProducts } from "../components/homeComponents/SpecialProducts";
+import VideoSection from "../components/homeComponents/VideoSection";
 
 export const Home = () => {
   // const api = import.meta.env.VITE_API_BASE_URL;
 
   return (
     <div>
+      <HeroSection />
       <FeaturesSection />
       {/* special products 1 */}
       <SpecialProducts title="منتجات خاصة" />
       <LapBanner />
+      <AdsSection />
+      <SlidersSection />
       {/* special products 2 */}
       <SpecialProducts title="منتجات مميزة " />
+      <VideoSection />
     </div>
   );
 };
