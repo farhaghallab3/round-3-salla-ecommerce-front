bf34320674a4f0698ee8dde8160cf5b9acbf7a3b
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-04-15 18:44:09 +0200

    add file testimonials

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from "react-router-dom";
 import HeaderNavbar from "../components/NavBar/navbar";
+import { Testimonials } from "../components/homeComponents/Testimonials";
 
 export const Layout = () => {
   return (
     <div>
       <HeaderNavbar />
       <Outlet />
+      <Testimonials />
     </div>
   );
 };
