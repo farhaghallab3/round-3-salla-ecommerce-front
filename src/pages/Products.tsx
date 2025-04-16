e75a6a2a5e888ca82ab187f15b12638f0f3aa84f
moustafaessam221 <moustafaessam991@gmail.com>
2025-04-16 22:05:15 +0200

    bug fixes

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,6 +2,8 @@ import { Filters } from "@/components/allProducts/Filters";
 import { ProductsList } from "@/components/allProducts/ProductsList";
 
 export const Products = () => {
+  // scroll to top on page load
+  window.scrollTo(0, 0);
   return (
     <div className="flex gap-6 flex-row responsive-padding-x">
       <Filters />
