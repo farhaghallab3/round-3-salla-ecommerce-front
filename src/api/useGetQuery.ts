a11035cde9fd3adf2b561ee5cb320bc44ec5f3fb
Hodayousry2752 <hanaayoussrey@gmail.com>
2025-04-21 05:25:09 +0200

    added api folder & added api at endSoon

---
+++ src/api/useGetQuery.ts
@@ -0,0 +1,26 @@
+import { useQuery, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import baseApi from './baseApi';
+
+export const useGetQuery = <T = any>(
+  key: string,
+  endpoint: string,
+  options?: Omit<UseQueryOptions<T, Error, T, [string, string]>, 'queryKey' | 'queryFn'>
+): UseQueryResult<T, Error> => {
+    console.log("Base URL is:", import.meta.env.VITE_API_BASE_URL);
+
+  return useQuery<T, Error, T, [string, string]>({
+    queryKey: [key, endpoint],
+    queryFn: async ({ queryKey }) => {
+      const [_key, _endpoint] = queryKey;
+      const res = await baseApi.get(_endpoint);
+      console.log("Request URL:", _endpoint);
+      console.log("Response data:", res.data);
+      return res.data as T;
+    },
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 65,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    ...options,
+  });
+};
